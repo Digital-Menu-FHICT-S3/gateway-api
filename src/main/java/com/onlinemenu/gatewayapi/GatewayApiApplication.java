0f902cb34f9825d9bc1292b5a694653a5622e5d2
milan <439008@student.fontys.nl>
2021-05-11 12:02:00 +0200

    Add globalcors

--- src/main/java/com/onlinemenu/gatewayapi/GatewayApiApplication.java
+++ src/main/java/com/onlinemenu/gatewayapi/GatewayApiApplication.java
@@ -3,9 +3,7 @@ package com.onlinemenu.gatewayapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,14 +13,4 @@ public class GatewayApiApplication {
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			public void addCorsMappings(CorsRegistry registry) {
-				registry
-						.addMapping("/**")
-						.allowedOrigins("*");
-			}
-		};
-	}
 }
